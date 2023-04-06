86936804fb2e752c73be20cf182dffa19fa28dd2
Daniela Villalva <dvillalva@ufl.edu>
2023-04-06 11:14:06 -0400

    added data-testids to increment and decrement.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -12,8 +12,8 @@ export default function Counter() {
     <div>
       <h2>Counter</h2>
       <p data-testid="count">{count}</p>
-      <button onClick={increment}> + </button>
-      <button onClick={decrement}> - </button>
+      <button data-testid="increase" onClick={increment}> + </button>
+      <button data-testid="decrease" onClick={decrement}> - </button>
     </div>
   );
 }
